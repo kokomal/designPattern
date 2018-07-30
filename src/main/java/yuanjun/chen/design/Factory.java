88c1abcb9a43a83506cd66eb4adf960c87bfb4b6
kokomal <mattleblanc@live.cn>
2018-07-30 15:32:35 +0800

    init

--- src/main/java/yuanjun/chen/design/Factory.java
+++ src/main/java/yuanjun/chen/design/Factory.java
@@ -1,7 +1,8 @@
 package yuanjun.chen.design;
+
 public class Factory {
 
-  public static void main(String[] args) {
-    System.out.println("hello factory");
-  }
+    public static void main(String[] args) {
+        System.out.println("hello factory");
+    }
 }
